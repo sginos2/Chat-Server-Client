9449c25a69c95aac434b288a51a49da22295062d
Shanna Ginos <Shanna@Dustins-MacBook-Pro.local>
2021-06-03 10:42:44 -0600

    changed users infoto use actual usernames, removed disconnected users from user array

--- server.js
+++ server.js
@@ -3,30 +3,39 @@ const fs = require('fs');
 const file = fs.createWriteStream('./chat.log');
 let users = [];
 const server = net.createServer((socket) => {
-    let welcome = `Welcome ${socket.remotePort} to the chat server!`;
+    let userNum = Math.ceil(Math.random() * 100);
+    let username = `User${userNum}`;
+    let welcome = `Welcome ${username} to the chat server!`;
+    socket.username = username;
     users.push(socket);
     file.write(`${welcome}\n`);
     socket.write(welcome);
     socket.setEncoding('utf-8');
     for(var i = 0; i < users.length; i++) {
-        if (users[i] !== socket){
+        if (users[i].username !== username){
             users[i].write(welcome);
         }
     }
     socket.on('data', (data) => {
-        file.write(`${socket.remotePort}: ${data}`);
-        console.log(`${socket.remotePort}: ${data}`);
+        file.write(`${username}: ${data}`);
+        console.log(`${username}: ${data}`);
         for(var i = 0; i < users.length; i++) {
-            if (users[i] !== socket){
-                users[i].write(`${socket.remotePort}: ${data}`);
+            if (users[i].username !== username){
+                users[i].write(`${username}: ${data}`);
             }
         }
     });
     socket.on('end', () => {
+
+        users = users.filter(user => {
+            return user.username !== username;
+        });
+
         for(var i = 0; i < users.length; i++) {
-            if (users[i] !== socket){
-                file.write(`${socket.remotePort} has disconnected.`);
-                users[i].write(`${socket.remotePort} has disconnected.`);
+            if (users[i] !== username){
+                file.write(`${username} has disconnected.`);
+                users[i].write(`${username} has disconnected.`);
+                console.log(users);
             }
         }
     })
