c3ffe52f8b8d3ab23c21a79f295715d3f87a19c2
Shanna Ginos <Shanna@Dustins-MacBook-Pro.local>
2021-06-09 11:09:29 -0600

    command server lab requriements: whisper function, clientlist function, and kick function complete, need to finish username function

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const net = require('net');
 const fs = require('fs');
 const file = fs.createWriteStream('./chat.log');
 let users = [];
+let adminPass = 'kicker3000';
 const server = net.createServer((socket) => {
     let userNum = Math.ceil(Math.random() * 100);
     let username = `User${userNum}`;
-    let welcome = `Welcome ${username} to the chat server!`;
+    let welcome = `Welcome ${username} to the chat server!\n`;
     socket.username = username;
     users.push(socket);
     file.write(`${welcome}\n`);
@@ -17,28 +18,158 @@ const server = net.createServer((socket) => {
         }
     }
     socket.on('data', (data) => {
-        file.write(`${username}: ${data}`);
-        console.log(`${username}: ${data}`);
-        for(var i = 0; i < users.length; i++) {
-            if (users[i].username !== username){
-                users[i].write(`${username}: ${data}`);
+        if (!data.startsWith('/')) {
+            file.write(`${username}: ${data}`);
+            console.log(`${username}: ${data}`);
+            for(var i = 0; i < users.length; i++) {
+                if (users[i].username !== username){
+                    users[i].write(`${username}: ${data}`);
+                }
             }
+        } else {
+            if (data.split(' ')[0].trim() === '/w') {
+                handleWhisper(data);
+            } else if (data.split(' ')[0].trim() === '/username') {
+                handleUsername(data);
+            } else if (data.split(' ')[0].trim() === '/kick') {
+                handleKick(data);
+            } else if (data.split(' ')[0].trim() === '/clientlist') {
+                handleList();
+            } 
         }
     });
-    socket.on('end', () => {
 
+    function handleWhisper(data){
+        let whisperer = username;
+        let whisperee = data.split(' ')[1].trim();
+        let msg = data.split(' ').slice(2).join(' ');
+        if (data.split(' ').length < 3) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === whisperer) {
+                    users[i].write('Incorrect number of inputs\n');
+                    file.write(`${username} wrote an incorrect number of inputs\n`);
+                }
+            }
+        } else if (whisperer === whisperee) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === whisperer) {
+                    users[i].write('You cannot whisper to yourself\n');
+                    file.write(`${username} tried to whisper to themselves\n`);
+                }
+            }
+        } else if (!users.find(user => user.username === whisperee)) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === whisperer) {
+                    users[i].write('Invalid username\n');
+                    file.write(`${username} wrote an invalid username\n`);
+                }
+            }
+        } else {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === whisperee) {
+                    users[i].write(`${username} whispered: ${msg}\n`);
+                    file.write(`${username} whispered to ${whisperee}: ${msg}\n`);
+                } 
+            }
+        }
+    };
+
+    // function handleUsername(data){
+    //     let newName = data.split(' ')[1].trim().toString();
+    //     if (data.split(' ').length < 2) {
+    //         for (let i = 0; i < users.length; i++) {
+    //             if (users[i].username === username) {
+    //                 users[i].write('Incorrect number of inputs\n');
+    //                 file.write(`${username} wrote an incorrect number of inputs\n`);
+    //             }
+    //         }
+    //     } else if (users.find(user => user.username === newName)) {
+    //         for (let i = 0; i < users.length; i++) {
+    //             if (users[i].username === username) {
+    //                 users[i].write('This username is taken\n');
+    //                 file.write(`${username} tried to steal someone else's username\n`);
+    //             }
+    //         }
+    //     } else {
+    //         for (let i = 0; i < users.length; i++) {
+    //             if (users[i].username === username) {
+    //                 users[i].username = newName;
+    //                 users[i].write(`Your username has been updated to ${newName}\n`);
+    //                 file.write(`${username}'s username has been updated to ${newName}\n`);
+    //             } 
+    //             users[i].write(`${username}'s username has been updated to ${newName}\n`);
+    //         }
+    //     }
+    // };
+
+    function handleKick(data){
+        let kicker = username;
+        let kickee = data.split(' ')[1].trim();
+        if (data.split(' ').length < 3) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === kicker) {
+                    users[i].write('Incorrect number of inputs\n');
+                    file.write(`${username} wrote an incorrect number of inputs\n`);
+                }
+            }
+        } else if (!users.find(user => user.username === kickee)) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === kicker) {
+                    users[i].write('Invalid username\n');
+                    file.write(`${username} wrote an invalid username\n`);
+                }
+            }
+        } else if (kicker === kickee) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === kicker) {
+                    users[i].write('You cannot kick yourself\n');
+                    file.write(`${username} tried to kick themselves\n`);
+                }
+            }
+        } else if (data.split(' ')[2].trim() !== adminPass) {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].username === kicker) {
+                    users[i].write('Incorrect password supplied\n');
+                    file.write(`${username} got the password wrong\n`);
+                }
+            }
+        } else {
+            let socketDestroy = users.find(user => user.username === kickee);
+            users = users.filter(user => {
+                return user.username !== kickee;
+            });
+            for(var i = 0; i < users.length; i++) {
+                users[i].write(`${username} has kicked ${kickee}.\n`);
+                file.write(`${username} has kicked ${kickee}\n`);
+            }
+            socketDestroy.destroy();
+        }
+    };
+
+    function handleList(){
+        let clientList = [];
+        for (let i = 0; i < users.length; i++) {
+            clientList.push(users[i].username);
+        }
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username === username) {
+                users[i].write(`${clientList}`);
+                file.write(`${username} accessed the Client List: ${clientList}\n`);
+            }
+        }
+    };
+    
+    socket.on('end', () => {
         users = users.filter(user => {
             return user.username !== username;
         });
-
         for(var i = 0; i < users.length; i++) {
             if (users[i] !== username){
-                file.write(`${username} has disconnected.`);
-                users[i].write(`${username} has disconnected.`);
-                console.log(users);
+                users[i].write(`${username} has disconnected.\n`);
+                file.write(`${username} has disconnected\n`);
             }
         }
-    })
+    });
 });
 
 server.listen(3000);
